d1dfc7e7199b6ec5883cb89ab5484b1c1045b07c
Amit <amitlandge333@gmail.com>
2022-10-31 16:45:33 +0530

    styling updated

--- script.js
+++
@@ -1,6 +0,0 @@
-const nav_list=document.querySelector(".nav_list");
-const bars=document.querySelector(".bars");
-bars.addEventListener("click",function () {
-    nav_list.classList.toggle("active");
-    console.log("hello");
-})
